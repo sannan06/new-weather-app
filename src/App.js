858e08b48af0e0ba5cbec2df11a7a1db29d920f5
sannan06 <sannanhafeez06@gmail.com>
2018-11-05 18:06:45 +1300

    Completed logic of application

--- src/App.js
+++ src/App.js
@@ -22,15 +22,25 @@ class App extends React.Component{
     const api_call = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city},${country}
     &appid=${API_KEY}`);
     const data = await api_call.json();
-    console.log(data);
-    this.setState({
-      temperature: data.main.temp,
-      city: data.name,
-      country: data.sys.country,
-      humidity: data.main.humidity,
-      description: data.weather[0].description,
-      error: ""
-    });
+    if(city && country){
+      this.setState({
+        temperature: data.main.temp,
+        city: data.name,
+        country: data.sys.country,
+        humidity: data.main.humidity,
+        description: data.weather[0].description,
+        error: ""
+      });
+    } else{
+      this.setState({
+        temperature: undefined,
+        city: undefined,
+        country: undefined,
+        humidity: undefined,
+        description: undefined,
+        error: "Please stop being difficult"
+      });
+    }
   }
 
   render(){
@@ -40,7 +50,7 @@ class App extends React.Component{
         <Form getWeather = {this.getWeather}/>
         <Weather 
           temperature = {this.state.temperature}
-          city = {this.state.country}
+          city = {this.state.city}
           country = {this.state.country}
           humidity = {this.state.humidity}
           description = {this.state.description}
--- src/Components/Titles.js
+++ src/Components/Titles.js
@@ -1,14 +1,9 @@
 import React from "react";
 
-class Titles extends React.Component{
-    render() {
-        return(
-            <div>
-                <h1>Weather Finder!</h1>
-                <p>Find your local weather conditions!</p>
-            </div>
-        );
-    }
-}
-
+const Titles = () => (
+    <div>
+        <h1>Weather Finder!</h1>
+        <p>Find your local weather conditions!</p>
+    </div>
+);
 export default Titles
